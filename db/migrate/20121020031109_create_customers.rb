3c10d47b4503f843b0c122d05e6f4146c5c17d03
jmtoporek <jmtoporek@gmail.com>
2012-10-20 22:45:33 -0500

    fixing migration file, keeping with sqlite

--- db/migrate/20121020031109_create_customers.rb
+++ db/migrate/20121020031109_create_customers.rb
@@ -2,7 +2,6 @@ class CreateCustomers < ActiveRecord::Migration
 	def change
 		create_table :customers do |t|
 			t.string :name
-			t.integer :customer_id
 			t.text :datastore
 
 			t.timestamps
--- db/migrate/20121020031625_create_locations.rb
+++ db/migrate/20121020031625_create_locations.rb
@@ -1,17 +1,16 @@
 class CreateLocations < ActiveRecord::Migration
-  def change
-    create_table :locations do |t|
-      #t.string :name
-	  t.string :store_number
-      t.string :street
-      t.string :city
-      t.integer :state_id
-      t.string :zipcode
-      t.string :phone
-	  t.integer :customer_id
-      t.text :datastore
+    def change
+        create_table :locations do |t|
+            t.string :store_number
+            t.string :street
+            t.string :city
+            t.integer :state_id
+            t.string :zipcode
+            t.string :phone
+            t.integer :customer_id
+            t.text :datastore
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
 end
